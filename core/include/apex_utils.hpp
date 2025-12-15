e11aa819735e465736ba0d3b2bd65c28261db8c3
Henrik Möllmann <henmoell@gmail.com>
2025-12-15 18:37:10 +0100

    Add GPRAT_START_TIMER and GPRAT_STOP_TIMER function

--- core/include/apex_utils.hpp
+++ core/include/apex_utils.hpp
@@ -1,8 +1,6 @@
 #ifndef APEX_STEPS_H
 #define APEX_STEPS_H
 
-#if GPRAT_APEX_STEPS
-
 #include <apex_api.hpp>
 #include <hpx/future.hpp>
 
@@ -16,19 +14,15 @@ inline double diff(const std::chrono::high_resolution_clock::time_point &start_t
 }
 
 /**
- * @brief Starts a timer for the next step.
- *
- * When GPRAT_APEX_STEPS=ON, this macro initializes a new apex timer newTimer for the current scope.
+ * @brief Initializes a new apex timer newTimer for the current scope.
  *
  * @param newTimer Identifier of the new timer variable to be declared
  */
 #define GPRAT_START_TIMER(newTimer) auto newTimer = now()
 
 /**
- * @brief Measures the duration of the previous step.
- *
- * When GPRAT_APEX_STEPS=ON, this macro blocks execution until all provided HPX futures are ready and samples the
- * duration of APEX timer oldTimer with label oldLabel.
+ * @brief Blocks execution until all provided HPX futures are ready and samples the duration of APEX timer oldTimer with
+ * label oldLabel.
  *
  * @param oldTimer Identifier of the existing timer variable to be sampled
  * @param oldLabel String label associated with the measured duration
@@ -38,12 +32,25 @@ inline double diff(const std::chrono::high_resolution_clock::time_point &start_t
     hpx::wait_all(__VA_ARGS__);                                                                                        \
     apex::sample_value(oldLabel, diff(oldTimer))
 
+// Macros GPRAT_START_STEP and GPRAT_END_STEP are conditionally defined based on the value of GPRAT_APEX_STEPS. They are
+// identical to GPRAT_START_TIMER and GPRAT_STOP_TIMER when GPRAT_APEX_STEPS=ON, otherwise they are defined as empty.
+#if GPRAT_APEX_STEPS
+
+/// @see GPRAT_START_TIMER
+#define GPRAT_START_STEP(newTimer) GPRAT_START_TIMER(newTimer)
+
+/// @see GPRAT_STOP_TIMER
+#define GPRAT_END_STEP(newTimer, oldTimer, oldLabel, ...) GPRAT_STOP_TIMER(newTimer, oldTimer, oldLabel, __VA_ARGS__)
+
 #else
 
 // Empty macro definitions when GPRAT_APEX_STEPS=OFF
-#define GPRAT_START_TIMER(...)
-#define GPRAT_STOP_TIMER(...)
+#define GPRAT_START_STEP(...)
+#define GPRAT_END_STEP(...)
 
 #endif  // GPRAT_APEX_STEPS
+
+// NOTE: We could also create similar macros, e.g. for GPRAT_APEX_CHOLESKY. However, since GPRAT_APEX_CHOLESKY is only
+// used in once, this would unnecessarily bloat this header file.
 
 #endif  // APEX_STEPS_H
